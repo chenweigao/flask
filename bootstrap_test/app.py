70db8dfc1c52c7384dd5676a899967dc1feade0d
chenweigao <297859260@qq.com>
2018-07-29 00:00:18 +0800

    add new branch

---
+++ bootstrap_test/app.py
@@ -0,0 +1,9 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
