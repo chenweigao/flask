821ebec7d312f7e059ee48a99422c10b90d81bcd
chenweigao <297859260@qq.com>
2018-07-30 21:38:26 +0800

    add new branch and "Flaks Moment", solved bootstrap problem.

--- smarttrack/smarttrack/__init__.py
+++ smarttrack/smarttrack/__init__.py
@@ -5,10 +5,10 @@ The flask application package.
 from flask import Flask
 from flask_pymongo import PyMongo
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 import smarttrack.views
 
  
 mongo = PyMongo(app, uri="mongodb://localhost:27017/runoobdb")
- 
+
