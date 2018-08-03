658cd9c79805fcac86496c4996a08a51ff369c49
chenweigao <297859260@qq.com>
2018-08-03 23:16:14 +0800

    update login and cookies

--- smarttrack/smarttrack/models.py
+++ smarttrack/smarttrack/models.py
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    # password is a write-only property
 
     @property
     def password(self):
